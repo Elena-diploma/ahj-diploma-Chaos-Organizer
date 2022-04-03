31a926e934c306f8e4232598b1a952e9303083fe
Elena Tursunova <elenatursunova@gmail.com>
2022-04-03 15:19:56 +0300

    Поправила порт

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const http = require('http');
 const path = require('path');
-const port = process.env.PORT || 8080;
+const port = parseInt(process.env.PORT) || 8080;
 const Koa = require('koa');
 const koaBody = require('koa-body');
 const cors = require('koa2-cors');
@@ -18,22 +18,6 @@ const app = new Koa();
 const router = new Router();
 const server = http.createServer(app.callback());
 const wsServer = new WS.Server({ server });
-
-//     {id: '255', message: 'text text text', created: new Date().toLocaleString('ru'), type: 'text'},
-//     {id: '258', message: 'testtest', created: new Date().toLocaleString('ru'), type: 'text'},
-//     {id: '275', message: 'link https://google.com https://meduza.io/', created: new Date().toLocaleString('ru'), type: 'link'},
-//     {id: '345', message: 'ocean.png', created: new Date().toLocaleString('ru'), type: 'image'},
-//     {id: '346', message: 'Sea - 24216.mp4', created: new Date().toLocaleString('ru'), type: 'video'},
-//     {id: '678', message: '00199.mp3', created: new Date().toLocaleString('ru'), type: 'audio'},
-//     {id: '679', message: '41118943.a4.pdf', created: new Date().toLocaleString('ru'), type: 'file'},
-//     {id: '301', message: 'text text text', created: new Date().toLocaleString('ru'), type: 'text'},
-//     {id: '105', message: 'Филаде́льфия (с др.-греч. Φιλαδέλφεια — любящая Дельфы) — древнеегипетский город, существовавший с 259—257 до н. э. до V—VI веков. Был основан в правление египетского царя Птолемея II Филадельфа и назван в честь жены и одновременно родной сестры Птолемея Арсинои Филадельфии.', created: new Date().toLocaleString('ru'), type: 'text'},
-//     {id: '564', message: 'Список творческих работ Олега Даля — список ролей, сыгранных актёром Олегом Далем в театре «Современник» и на других сценических площадках страны', created: new Date().toLocaleString('ru'), type: 'text'},
-//     {id: '444', message: 'Рубль перешел к росту против евро и стабилен к доллару', created: new Date().toLocaleString('ru'), type: 'text'},
-//     {id: '454', message: 'https://netology.ru', created: new Date().toLocaleString('ru'), type: 'text'},
-//     {id: '754', message: '55.89660, 37.61168', created: new Date().toLocaleString('ru'), type: 'geo'},
-// ];
-
 
 app.use(
     cors({
